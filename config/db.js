c5f815ed8ea0d5bf9fec1056c1e689f4977c4143
Xerox212 <arunpreet37@gmail.com>
2016-03-17 18:20:29 -0400

    first commit

---
+++ config/db.js
@@ -0,0 +1,9 @@
+// create public connection string
+module.exports = {
+
+    // local
+    //'url': 'mongodb://localhost/test'
+
+    // live on mLab
+    'url': 'mongodb://gc200299028:123456q@ds064278.mlab.com:64278/student200299028'
+};
